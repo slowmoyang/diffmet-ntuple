70417fecec86e49eea430f6d6f16f38412fc2e29
Seungjin Yang <slowmoyang@gmail.com>
2024-10-19 16:49:49 +0900

    Make the feature list similar to DeepMET
    - make the feature list similar to DeepMET. see https://cds.cern.ch/record/2744871/
    - add a test mode

--- ntupleise.py
+++ ntupleise.py
@@ -2,6 +2,7 @@
 """
 """
 import argparse
+from enum import Enum
 from typing import Final
 from pathlib import Path
 import numpy as np
@@ -12,8 +13,9 @@ import h5py as h5
 import tqdm
 
 
-ELECTRON_PID: Final[int] = 11
-MUON_PID: Final[int] = 13
+# FIXME:
+PROJECT_DATA_DIR = Path('/store/hep/users/slowmoyang/KSC-RnD-2024/')
+
 
 BRANCH_LIST: Final[list[str]] = [
     # EFlowTrack
@@ -23,6 +25,8 @@ BRANCH_LIST: Final[list[str]] = [
     "EFlowTrack/EFlowTrack.PID",
     "EFlowTrack/EFlowTrack.Charge",
     "EFlowTrack/EFlowTrack.IsRecoPU", # TODO
+    "EFlowTrack/EFlowTrack.D0",
+    "EFlowTrack/EFlowTrack.DZ",
     # EFlowPhoton
     "EFlowPhoton/EFlowPhoton.ET",
     "EFlowPhoton/EFlowPhoton.Eta",
@@ -62,6 +66,7 @@ FEATURE_ALIAS_DICT: Final[dict[str, str]] = {
     'ET': 'PT',
     'MET': 'PT',
     'IsRecoPU': 'is_reco_pu',
+    'PID': 'pdgid',
 }
 
 
@@ -78,9 +83,44 @@ def make_alias(
     return alias
 
 
+class ParticleTypeIndex(Enum):
+    ZERO_PAD = 0
+    ELECTRON = 1
+    MUON = 2
+    CHARGED_HADRON = 3
+    PHOTON = 4
+    NEUTRAL_HADRON = 5
+
+
+class PDGId(Enum):
+    ELECTRON = 11
+    MUON = 13
+
+
+
+def _convert_track_pdgid_to_pid(pdgid_arr):
+    pdgid_arr = ak.to_numpy(np.abs(pdgid_arr))
+
+    electron_mask = pdgid_arr == PDGId.ELECTRON.value
+    muon_mask = pdgid_arr == PDGId.MUON.value
+
+    pid_arr = np.full_like(
+        pdgid_arr,
+        fill_value=ParticleTypeIndex.CHARGED_HADRON.value,
+    )
+
+    pid_arr[electron_mask] = PDGId.ELECTRON.value
+    pid_arr[muon_mask] = PDGId.MUON.value
+
+    return pid_arr
+
+def convert_track_pdgid_to_pid(pdgid_arr):
+    return ak.Array(map(_convert_track_pdgid_to_pid, pdgid_arr))
+
+
 def run(
-    input_path_list: list[Path],
-    output_path: Path,
+    input_file_path_list: list[Path],
+    output_file_path: Path,
     input_treepath: str = 'Delphes',
     input_branch_list: list[str] = BRANCH_LIST,
 ) -> None:
@@ -90,10 +130,10 @@ def run(
                                for each in input_branch_list}
     expressions: list[str] = list(aliases.keys())
 
-    input_files = {each: input_treepath for each in input_path_list}
+    input_files = {each: input_treepath for each in input_file_path_list}
     total = sum(num_entries for *_, num_entries in uproot.num_entries(input_files))
 
-    output_file = h5.File(output_path, 'w')
+    output_file = h5.File(output_file_path, 'w')
 
     def create_dataset(name, dtype: type = np.float32, vlen: bool = False):
         """create variable-length dataset"""
@@ -103,27 +143,51 @@ def run(
 
     output = {}
 
-    track_suffix_list = ['px', 'py', 'eta', 'is_electron', 'is_muon',
-                         'is_hadron', 'charge', 'is_reco_pu']
+    ###########################################################################
+    # createt datasets for tracks
+    ###########################################################################
+    track_float32_suffix_list = [
+        'px', 'py', 'eta', 'd0', 'dz'
+    ]
+
+    track_int64_suffix_list = [
+        'is_reco_pu', 'charge', 'pid',
+    ]
+    track_suffix_list = track_float32_suffix_list + track_int64_suffix_list
     for suffix in track_suffix_list:
         key = f'track_{suffix}'
-        dtype = np.float32 if suffix in ['px', 'py', 'eta'] else np.int64
+        dtype = np.float32 if suffix in track_float32_suffix_list else np.int64
         output[key] = create_dataset(key, dtype, vlen=True)
 
-    tower_suffix_list = ['px', 'py', 'eta', 'is_hadron']
+    ###########################################################################
+    # create datasets for towers
+    ###########################################################################
+    tower_float32_suffix_list = ['px', 'py', 'eta']
+    tower_int64_suffix_list = ['pid']
+    tower_suffix_list = tower_float32_suffix_list + tower_int64_suffix_list
+
     for suffix in tower_suffix_list:
         key = f'tower_{suffix}'
-        dtype = np.float32 if suffix in ['px', 'py', 'eta'] else np.int64
+        dtype = np.float32 if suffix in tower_float32_suffix_list else np.int64
         output[key] = create_dataset(key, dtype, vlen=True)
 
+    ###########################################################################
+    # create datasets for various types of METs
+    ###########################################################################
     for obj in ['pf_met', 'puppi_met', 'gen_met', 'gen_pileup_met']:
         for feature in ['pt', 'phi']:
             key = f'{obj}_{feature}'
             output[key] = create_dataset(key)
 
+    ###########################################################################
+    #
+    ###########################################################################
     for key in ['gen_weight']:
         output[key] = create_dataset(key)
 
+    ###########################################################################
+    # fill datasets
+    ###########################################################################
     start = 0
     stop = 0
 
@@ -139,38 +203,65 @@ def run(
             stop = start + num_entries
             slicing = slice(start, stop)
 
-            output['track_px'][slicing] = chunk['track_pt'] * np.cos(chunk['track_phi'])
-            output['track_py'][slicing] = chunk['track_pt'] * np.sin(chunk['track_phi'])
+            ####################################################################
+            # track
+            ####################################################################
+            track_pt = chunk['track_pt'] # type: ignore
+            track_phi = chunk['track_phi'] # type: ignore
+            output['track_px'][slicing] = track_pt * np.cos(track_phi)
+            output['track_py'][slicing] = track_pt * np.sin(track_phi)
 
             for suffix in ['eta', 'is_reco_pu', 'charge']:
                 key = f'track_{suffix}'
-                output[key][slicing] = chunk[key]
+                output[key][slicing] = chunk[key] # type: ignore
 
-            track_abs_pid = np.abs(chunk['track_pid'])
-            output['track_is_electron'][slicing] = track_abs_pid == ELECTRON_PID
-            output['track_is_muon'][slicing] = track_abs_pid == MUON_PID
-            output['track_is_hadron'][slicing] = (track_abs_pid != ELECTRON_PID) & (track_abs_pid != MUON_PID)
+            output['track_pid'][slicing] = convert_track_pdgid_to_pid(
+                pdgid_arr=chunk['track_pdgid'], # type: ignore
+            )
 
+            ###################################################################
+            # tower
+            # photon first
+            ###################################################################
             for feature in ['pt', 'eta', 'phi']:
-                arrays = [ak.values_astype(chunk[f'{prefix}_{feature}'], np.float32)
-                          for prefix in ['neutral_hadron', 'photon']]
-                chunk[f'tower_{feature}'] = ak.concatenate(
+                arrays = [
+                    ak.values_astype(
+                        array=chunk[f'{prefix}_{feature}'],  # type: ignore
+                        to=np.float32
+                    )
+                    for prefix in ['photon', 'neutral_hadron']
+                ]
+                chunk[f'tower_{feature}'] = ak.concatenate( # type: ignore
                     arrays=arrays,
                     axis=1
                 )
 
-            output['tower_px'][slicing] = chunk['tower_pt'] * np.cos(chunk['tower_phi'])
-            output['tower_py'][slicing] = chunk['tower_pt'] * np.sin(chunk['tower_phi'])
+            tower_pt = chunk['tower_pt'] # type: ignore
+            tower_phi = chunk['tower_phi'] # type: ignore
+            output['tower_px'][slicing] = tower_pt * np.cos(tower_phi)
+            output['tower_py'][slicing] = tower_pt * np.sin(tower_phi)
+            output['tower_eta'][slicing] = chunk['tower_eta'] # type: ignore
 
-            output['tower_eta'][slicing] = chunk['tower_eta']
-            output['tower_is_hadron'][slicing] = ak.concatenate(
+            output['tower_pid'][slicing] = ak.concatenate(
                 arrays=[
-                    ak.ones_like(chunk['neutral_hadron_pt'], dtype=np.int64),
-                    ak.zeros_like(chunk['photon_pt'], dtype=np.int64)
+                    ak.full_like(
+                        array=chunk['photon_pt'],  # type: ignore
+                        fill_value=ParticleTypeIndex.PHOTON.value,
+                        dtype=np.int64
+                    ),
+                    ak.full_like(
+                        array=chunk['neutral_hadron_pt'], # type: ignore
+                        fill_value=ParticleTypeIndex.NEUTRAL_HADRON.value,
+                        dtype=np.int64
+                    ),
+
                 ],
                 axis=1,
             )
 
+            ###################################################################
+            #
+            ###################################################################
             for obj in ['pf_met', 'puppi_met', 'gen_met', 'gen_pileup_met']:
                 for feature in ['pt', 'phi']:
                     key = f'{obj}_{feature}'
@@ -190,6 +281,24 @@ def run(
 
     output_file.close()
 
+
+def run_test(
+    input_dir_path: Path,
+    output_file_path: Path,
+    max_input_files: int | None,
+) -> None:
+    """
+    """
+    input_file_path_list = list(input_dir_path.glob('*.root'))
+    input_file_path_list = input_file_path_list[:max_input_files]
+
+    run(
+        input_file_path_list=input_file_path_list,
+        output_file_path=output_file_path
+    )
+
+
+
 def run_batch(
     dataset: str,
     counter: int,
@@ -197,29 +306,28 @@ def run_batch(
 ) -> None:
     """
     """
-    root = Path('/store/hep/users/slowmoyang/KSC-RnD-2024/')
 
     counter_label = f'{counter:06d}'
 
-    input_dir = root / 'delphes' / delphes_config / dataset / counter_label
+    input_dir = PROJECT_DATA_DIR / 'delphes' / delphes_config / dataset / counter_label
     if not input_dir.exists():
         raise FileNotFoundError(input_dir)
 
-    input_path_list = sorted(
+    input_file_path_list = sorted(
         input_dir.glob('*.root'),
         key=lambda item: int(item.stem.removeprefix('output_'))
     )
 
-    output_dir = root / 'ntuple' / delphes_config / dataset
+    output_dir = PROJECT_DATA_DIR / 'ntuple' / delphes_config / dataset
     if not output_dir.exists():
         output_dir.mkdir(parents=True)
 
-    output_path = output_dir / counter_label
-    output_path = output_path.with_suffix('.h5')
+    output_file_path = output_dir / counter_label
+    output_file_path = output_file_path.with_suffix('.h5')
 
     run(
-        input_path_list=input_path_list,
-        output_path=output_path
+        input_file_path_list=input_file_path_list,
+        output_file_path=output_file_path
     )
 
 
@@ -229,15 +337,38 @@ def main():
     )
     subparsers = parser.add_subparsers()
 
-    batch_parser = subparsers.add_parser('batch')
-    batch_parser.add_argument('-d', '--dataset', type=str, required=True,
-                              help='dataset')
+
+    test_parser = subparsers.add_parser(
+        name='test',
+        help='test mode',
+    )
+    test_parser.set_defaults(func=run_test)
+    test_parser.add_argument(
+        '-i', '--input-dir-path', type=Path, required=True,
+        help='input directory',
+    )
+    test_parser.add_argument(
+        '-o', '--output-file-path', type=Path, required=True,
+        help='output file',
+    )
+    test_parser.add_argument(
+        '-m', '--max-input-files', default=10, type=int,
+        help='output file',
+    )
+
+    batch_parser = subparsers.add_parser(
+        name='batch',
+        help='batch mode',
+    )
+    batch_parser.set_defaults(func=run_batch)
+    batch_parser.add_argument(
+        '-d', '--dataset', type=str, required=True,
+        help='dataset')
     batch_parser.add_argument('-c', '--counter', type=int, required=True,
                               help='counter')
     batch_parser.add_argument('--delphes-config', type=str,
                               default='CMS_PhaseII_200PU_v04',
                               help='delphes config')
-    batch_parser.set_defaults(func=run_batch)
 
     args = parser.parse_args()
     args = vars(args)
